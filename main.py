193eb039051d0703f7719e5e2851c297f0cdbbe2
grafkinlm-com <grafkinlm@gmail.com>
2026-03-25 18:35:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Initialize bot and dispatcher
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=8251658039:AAHg__fHz5fSkeYeI9PFby7aI4IUYRKQnxE)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
